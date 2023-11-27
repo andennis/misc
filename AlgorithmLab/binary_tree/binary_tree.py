274b66b06187331e256153d8101bcfd9ae2e2eee
Dzianis Andreyeu <andninbox@gmail.com>
2023-11-27 17:41:24 +0300

    AlgorithLab - (asymmetric tree - best performance connect_right_nodes_v3) Populating Next Right Pointers in Each Node

--- AlgorithmLab/binary_tree/binary_tree.py
+++ AlgorithmLab/binary_tree/binary_tree.py
@@ -1,4 +1,5 @@
 from typing import Optional, List
+from collections import deque
 
 
 class TreeNode:
@@ -423,3 +424,26 @@ class TreeNodeTraversal:
                         node = node.next
 
         return root
+
+    @staticmethod
+    def connect_right_nodes_v3(root: TreeNode) -> TreeNode:
+        """Here is the best performance version from leetcode"""
+        if not root:
+            return root
+
+        q = deque()
+        q.append(root)
+        while q:
+            n = len(q)
+            for i in range(n):
+                node = q.popleft()
+
+                if i < n - 1:
+                    node.next = q[0]
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return root
+
