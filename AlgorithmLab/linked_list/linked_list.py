fa03d72a0c711d5e5d35336b23678e36b1afb062
Dzianis Andreyeu <andninbox@gmail.com>
2023-12-01 19:40:44 +0300

    AlgorithLab - Get middle node of a linked list

--- AlgorithmLab/linked_list/linked_list.py
+++ AlgorithmLab/linked_list/linked_list.py
@@ -1,14 +1,32 @@
+from typing import Optional
+
+
 # Delete a node from a singly-linked list, ↴ given only a variable pointing to that node.
-class LinkedListNode(object):
+class ListNode(object):
 
     def __init__(self, value, next_node=None):
-        self.value = value
+        self.val = value
         self.next = next_node
 
 
-def delete_node(node: LinkedListNode):
+def delete_node(node: ListNode):
     next_node = node.next
     if not next_node:
         raise Exception("The last node cannot be deleted")
-    node.value = next_node.value
+    node.val = next_node.val
     node.next = next_node.next
+
+
+def middle_node(head: Optional[ListNode]) -> Optional[ListNode]:
+    def nodes_count(node: Optional[ListNode], node_index: int) -> tuple[Optional[ListNode], int]:
+        if not node:
+            return None, node_index
+
+        next_node, count = nodes_count(node.next, node_index + 1)
+        if not next_node and node_index == count // 2:
+            return node, node_index
+
+        return next_node, count
+
+    result = nodes_count(head, 0)
+    return result[0]
--- AlgorithmLab/linked_list/test_linked_list.py
+++ AlgorithmLab/linked_list/test_linked_list.py
@@ -1,27 +1,39 @@
 import pytest
-from linked_list import delete_node, LinkedListNode
+from linked_list import delete_node, ListNode, middle_node
 
 
 def test_delete_first_node():
-    c = LinkedListNode('C')
-    b = LinkedListNode('B', c)
-    a = LinkedListNode('A', b)
+    c = ListNode('C')
+    b = ListNode('B', c)
+    a = ListNode('A', b)
     delete_node(a)
-    assert a.value == 'B'
-    assert a.next.value == 'C'
+    assert a.val == 'B'
+    assert a.next.val == 'C'
 
 
 def test_delete_middle_node():
-    c = LinkedListNode('C')
-    b = LinkedListNode('B', c)
-    a = LinkedListNode('A', b)
+    c = ListNode('C')
+    b = ListNode('B', c)
+    a = ListNode('A', b)
 
     delete_node(b)
-    assert a.next.value == 'C'
+    assert a.next.val == 'C'
 
 
 def test_delete_last_node():
-    b = LinkedListNode('B')
-    a = LinkedListNode('A', b)
+    b = ListNode('B')
+    a = ListNode('A', b)
     with pytest.raises(Exception):
         delete_node(b)
+
+
+@pytest.mark.parametrize("head, result", [
+    (None, None),
+    (ListNode("A"), "A"),
+    (ListNode("A", ListNode("B")), "B"),
+    (ListNode("A", ListNode("B", ListNode("C"))), "B"),
+    (ListNode("A", ListNode("B", ListNode("C", ListNode("D")))), "C")
+])
+def test_middle_node(head, result):
+    node = middle_node(head)
+    assert node.val if node else None == result
