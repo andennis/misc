6fe47c4cd75df2d97eaa55e611f34a0b2789db61
Dzianis Andreyeu <andninbox@gmail.com>
2023-12-07 21:15:16 +0300

    AlgorithLab - hashing.find_max_length_v2

--- AlgorithmLab/Hashing/hashing.py
+++ AlgorithmLab/Hashing/hashing.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def find_max_length(nums: List[int]) -> int:
+def find_max_length_v1(nums: List[int]) -> int:
     d = {0: 0}
     max_ln = 0
     sm = 0
@@ -14,5 +14,21 @@ def find_max_length(nums: List[int]) -> int:
                 max_ln = ln
         else:
             d[i - 2 * sm] = i
+
+    return max_ln
+
+
+def find_max_length_v2(nums: List[int]) -> int:
+    max_ln = 0
+    n = len(nums)
+    arr = [-2] * (n * 2 + 1)
+    arr[n] = -1
+    count = 0
+    for i in range(n):
+        count += 1 if nums[i] == 1 else -1
+        if arr[count + n] >= -1:
+            max_ln = max(max_ln, i - arr[count + n])
+        else:
+            arr[count + n] = i
 
     return max_ln
--- AlgorithmLab/Hashing/test_hashing.py
+++ AlgorithmLab/Hashing/test_hashing.py
@@ -1,8 +1,10 @@
 import pytest
-from hashing import find_max_length
+from hashing import find_max_length_v1, find_max_length_v2
 
 
 @pytest.mark.parametrize("nums, result", [
+    ([1, 0, 1], 2),
+    ([1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1], 12),
     ([0, 1, 1], 2),
     ([0, 1, 0], 2),
     ([0, 1], 2),
@@ -10,4 +12,4 @@ from hashing import find_max_length
     ([0, 0, 1, 0, 0, 0, 1, 1], 6)
 ])
 def test_find_max_length(nums, result):
-    assert find_max_length(nums) == result
+    assert find_max_length_v2(nums) == result
