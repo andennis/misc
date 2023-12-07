16ac71582a963b7cc39a6901708ee936c740fee6
Dzianis Andreyeu <andninbox@gmail.com>
2023-12-07 20:40:50 +0300

    AlgorithLab - hashing.find_max_length improved

--- AlgorithmLab/Hashing/hashing.py
+++ AlgorithmLab/Hashing/hashing.py
@@ -2,19 +2,17 @@ from typing import List
 
 
 def find_max_length(nums: List[int]) -> int:
-    prefix = [0]
-    for i in range(len(nums)):
-        prefix.append(nums[i] + prefix[i])
-
     d = {0: 0}
     max_ln = 0
-    for i in range(1, len(prefix)):
-        v = i - 2 * prefix[i]
+    sm = 0
+    for i in range(1, len(nums) + 1):
+        sm += nums[i-1]
+        v = i - 2 * sm
         if v in d:
             ln = i - d[v]
             if ln > max_ln:
                 max_ln = ln
         else:
-            d[i - 2 * prefix[i]] = i
+            d[i - 2 * sm] = i
 
     return max_ln
